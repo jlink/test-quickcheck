1b2a82a9fad46ee9d80cdb1b5c54361e3d215c7a
Johannes Link <business@johanneslink.net>
2015-10-09 08:06:13 +0200

    Initial commit

---
+++ src/test/java/test1/StringTheories.java
@@ -0,0 +1,18 @@
+package test1;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Theories.class)
+public class StringTheories {
+    @Theory public void concatenationLength(
+            @ForAll String s1,
+            @ForAll String s2) {
+
+        assertEquals(s1.length() + s2.length(), (s1 + s2).length());
+    }
+}
